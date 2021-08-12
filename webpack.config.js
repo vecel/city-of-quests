d88dfd4ac715507a6bb9b1ea8aa5b08df753f876
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-08-12 14:12:02 +0200

    Install eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            chunks: ['main'],
-            template: './src/index.html',
-        })
-    ],
-}
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['main'],
+      template: './src/index.html',
+    }),
+  ],
+};
