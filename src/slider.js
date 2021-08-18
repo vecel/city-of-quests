ce643ff17f1aa644e027fcb4ce03bde1c7083f16
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-08-18 22:14:53 +0200

    Code refactoring

--- src/slider.js
+++ src/slider.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 const slider = document.querySelector('div#slider');
 
 const slides = slider.querySelectorAll('.slide');
@@ -7,42 +6,36 @@ const nextSlide = slider.querySelector('button.next');
 const bottomNav = slider.querySelectorAll('ul#slider-bottom-nav li');
 
 let activeSlide = 0;
-let sliderInterval = setInterval(() => showSlide(activeSlide + 1), 5000);
-
-const showSlide = function (n) {
-    let slideToShow = n;
-    if (n < 0) {
-        slideToShow = slides.length - 1;
-    }
-    if (n >= slides.length) {
-        slideToShow = 0;
-    }
-    switchSlides(slideToShow);
-    highlightNavigationDot(slideToShow);
-    activeSlide = slideToShow;
-    sliderInterval = resetInterval(sliderInterval, () => showSlide(activeSlide + 1), 5000);
-};
+let sliderInterval;
 
 const switchSlides = function (slideToShow) {
     slides[activeSlide].classList.remove('active-slide');
     slides[slideToShow].classList.add('active-slide');
-}
+};
 
-const highlightNavigationDot = function (slideToShow) {
+const highlightNavDot = function highlightBottomNavigationDot(slideToShow) {
     bottomNav[activeSlide].classList.remove('active');
     bottomNav[slideToShow].classList.add('active');
-}
+};
 
 const resetInterval = function (intervalId, callback, delay) {
     clearInterval(intervalId);
     return setInterval(callback, delay);
-}
+};
+
+const showSlide = function showSlideNumberN(n) {
+    let slideToShow = n;
+    if (n < 0) slideToShow = slides.length - 1;
+    if (n >= slides.length) slideToShow = 0;
+
+    switchSlides(slideToShow);
+    highlightNavDot(slideToShow);
+    activeSlide = slideToShow;
+    sliderInterval = resetInterval(sliderInterval, () => showSlide(activeSlide + 1), 5000);
+};
 
 previousSlide.addEventListener('click', () => showSlide(activeSlide - 1));
 nextSlide.addEventListener('click', () => showSlide(activeSlide + 1));
-
-for (const [i, navDot] of bottomNav.entries()) {
-    navDot.addEventListener('click', showSlide.bind(this, i));
-}
+bottomNav.forEach((navDot, i) => navDot.addEventListener('click', showSlide.bind(this, i)));
 
 showSlide(activeSlide);
